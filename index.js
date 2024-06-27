72b1314ff44a75f7e240d8b6d3792e3387b22038
Albert Ng <albnok@gmail.com>
2024-06-27 18:16:24 +0800

    enforce update revalidate length

--- index.js
+++ index.js
@@ -92,17 +92,18 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  const { content, important } = request.body
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
     .then(updatedNote => {
       response.json(updatedNote)
     })
     .catch(error => next(error))
 })
+
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
